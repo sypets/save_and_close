9ecdf4699ac9fa11d1abd672a6cf04e32bb6f9fa
Sybille Peters <sypets@gmx.de>
2024-12-27 14:09:09 +0100

    [TASK] Make ready for v13
    - replace Icon size constants

--- Classes/EventListener/AddButton.php
+++ Classes/EventListener/AddButton.php
@@ -7,6 +7,8 @@ use TYPO3\CMS\Backend\Template\Components\ModifyButtonBarEvent;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Imaging\IconSize;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Backend\Template\Components\Buttons\InputButton;
@@ -26,6 +28,21 @@ class AddButton
         $buttons = $event->getButtons();
         $buttonBar = $event->getButtonBar();
         $saveButton = $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][0] ?? null;
+
+        /** @var Typo3Version $typoVersion */
+        $typoVersion = GeneralUtility::makeInstance(Typo3Version::class);
+        /**
+         * Due to deprecation in TYPO3 v13
+         * @see https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/13.0/Deprecation-101475-IconSizeStringConstants.html
+         *
+         * @todo Switch can be removed when support for v12 is dropped
+         */
+        if ($typoVersion->getMajorVersion() < 13) {
+            $iconSize = Icon::SIZE_SMALL;
+        } else {
+            $iconSize = IconSize::SMALL;
+        }
+
         if ($saveButton instanceof InputButton) {
             /** @var IconFactory $iconFactory */
             $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
@@ -36,7 +53,7 @@ class AddButton
                     ->setValue('1')
                     ->setForm($saveButton->getForm())
                     ->setTitle($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveCloseDoc'))
-                    ->setIcon($iconFactory->getIcon('actions-document-save-close', Icon::SIZE_SMALL))
+                    ->setIcon($iconFactory->getIcon('actions-document-save-close', $iconSize))
                     ->setShowLabelText(true);
                 $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][] = $saveCloseButton;
             }
@@ -47,7 +64,7 @@ class AddButton
                     ->setValue('1')
                     ->setForm($saveButton->getForm())
                     ->setTitle($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveDocShow'))
-                    ->setIcon($iconFactory->getIcon('actions-document-save-view', Icon::SIZE_SMALL))
+                    ->setIcon($iconFactory->getIcon('actions-document-save-view', $iconSize))
                     ->setShowLabelText(true);
                 $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][] = $saveViewButton;
             }
